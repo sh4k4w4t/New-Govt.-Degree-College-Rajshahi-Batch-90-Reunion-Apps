12f46ba4ab74f1adeb8fad9049845d7de7dcbc07
shakawat <shakawat_hossain@outlook.com>
2022-07-01 22:44:16 +0600

    just code update

--- app/src/main/java/ngdc/rajshahi/reunion/anotherHomeActivity/becomeAmember/Become_a_member_activity.java
+++ app/src/main/java/ngdc/rajshahi/reunion/anotherHomeActivity/becomeAmember/Become_a_member_activity.java
@@ -12,9 +12,24 @@ import android.widget.Toast;
 import ngdc.rajshahi.reunion.R;
 
 public class Become_a_member_activity extends AppCompatActivity {
-    EditText formPasswordConfirm,formPassword,formName,formEmail,formMobile,formPresentAddress,formPermanentAddress,formGroup,formNameFather,formNameMother,formNameSpouse,formNumberOfChild,formOccupation,formInstituteName,formDesignation,formOfficeAddress,formMemberType;
+    EditText formPasswordConfirm,
+            formPassword,
+            formName,
+            formEmail,
+            formMobile,
+            formPresentAddress,
+            formPermanentAddress,
+            formGroup,
+            formNameFather,
+            formNameMother,
+            formNameSpouse,
+            formNumberOfChild,
+            formOccupation,
+            formInstituteName,
+            formDesignation,
+            formOfficeAddress,
+            formMemberType;
     Button formSubmitButton;
-    String formPasswordConfirmCollected,formPasswordCollected,formMemberTypeCollected,formOfficeAddressCollected,formInstituteNameCollected,formOccupationCollected,formDesignationCollected,formNumberOfChildCollected,formNameSpouseCollected,formNameMotherCollected,formNameFatherCollected,formGroupCollected,formPermanentAddressCollected,formNameCollected,formEmailCollected,formMobileCollected,formPresentAddressCollected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,56 +40,72 @@ public class Become_a_member_activity extends AppCompatActivity {
         setContentView(R.layout.activity_become_amember);
         this.setTitle("Become a Member");
         utilizedUI();
-        submitData();
-    }
 
-    private void submitData() {
+        String formNameCollected = formName.getText().toString().trim();
+        String formEmailCollected = formEmail.getText().toString().trim();
+        String formMobileCollected = formMobile.getText().toString().trim();
+        String formPresentAddressCollected = formPresentAddress.getText().toString().trim();
+        String formPermanentAddressCollected = formPermanentAddress.getText().toString().trim();
+        String formGroupCollected = formGroup.getText().toString().trim();
+        String formNameFatherCollected = formNameFather.getText().toString().trim();
+        String formNameMotherCollected = formNameMother.getText().toString().trim();
+        String formNameSpouseCollected = formNameSpouse.getText().toString().trim();
+        String formNumberOfChildCollected = formNumberOfChild.getText().toString().trim();
+        String formOccupationCollected = formOccupation.getText().toString().trim();
+        String formInstituteNameCollected = formInstituteName.getText().toString().trim();
+        String formDesignationCollected = formDesignation.getText().toString().trim();
+        String formOfficeAddressCollected = formOfficeAddress.getText().toString().trim();
+        String formMemberTypeCollected = formMemberType.getText().toString().trim();
+        String formPasswordCollected = formPassword.getText().toString().trim();
+        String formPasswordConfirmCollected = formPasswordConfirm.getText().toString().trim();
+
         formSubmitButton.setOnClickListener(view -> {
-            collectInfoFromForm();
-            if (formNameCollected.equals("")||formEmailCollected.equals("")||formMobileCollected.equals("")||formPresentAddressCollected.equals("")||formPermanentAddressCollected.equals("")||formGroupCollected.equals("")||formNameFatherCollected.equals("")||formNameMotherCollected.equals("")||formNameSpouseCollected.equals("")||formNumberOfChildCollected.equals("")||formOccupationCollected.equals("")||formInstituteNameCollected.equals("")||formDesignationCollected.equals("")||formOfficeAddressCollected.equals("")||formMemberTypeCollected.equals("")){
-                Toast.makeText(this, "Fill up all information", Toast.LENGTH_SHORT).show();
-            }else {
+            if (formNameCollected.equals("") ||
+                    formEmailCollected.equals("") ||
+                    formMobileCollected.equals("") ||
+                    formPresentAddressCollected.equals("") ||
+                    formPermanentAddressCollected.equals("") ||
+                    formGroupCollected.equals("") ||
+                    formNameFatherCollected.equals("") ||
+                    formNameMotherCollected.equals("") ||
+                    formNameSpouseCollected.equals("") ||
+                    formNumberOfChildCollected.equals("") ||
+                    formOccupationCollected.equals("") ||
+                    formInstituteNameCollected.equals("") ||
+                    formDesignationCollected.equals("") ||
+                    formOfficeAddressCollected.equals("") ||
+                    formPasswordCollected.equals("") ||
+                    formPasswordConfirmCollected.equals("") ||
+                    formMemberTypeCollected.equals("")) {
+                if (!formPasswordCollected.equals(formPasswordConfirmCollected)) {
+                    Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Fill up all information", Toast.LENGTH_SHORT).show();
+                }
+            } else {
                 Toast.makeText(this, "Successful submit your data", Toast.LENGTH_SHORT).show();
             }
         });
     }
-    private void collectInfoFromForm() {
-        formNameCollected =formName.getText().toString().trim();
-        formEmailCollected=formEmail.getText().toString().trim();
-        formMobileCollected=formMobile.getText().toString().trim();
-        formPresentAddressCollected=formPresentAddress.getText().toString().trim();
-        formPermanentAddressCollected=formPermanentAddress.getText().toString().trim();
-        formGroupCollected=formGroup.getText().toString().trim();
-        formNameFatherCollected=formNameFather.getText().toString().trim();
-        formNameMotherCollected=formNameMother.getText().toString().trim();
-        formNameSpouseCollected=formNameSpouse.getText().toString().trim();
-        formNumberOfChildCollected=formNumberOfChild.getText().toString().trim();
-        formOccupationCollected=formOccupation.getText().toString().trim();
-        formInstituteNameCollected=formInstituteName.getText().toString().trim();
-        formDesignationCollected=formDesignation.getText().toString().trim();
-        formOfficeAddressCollected=formOfficeAddress.getText().toString().trim();
-        formMemberTypeCollected=formMemberType.getText().toString().trim();
-        formPasswordCollected=formPassword.getText().toString().trim();
-        formPasswordConfirmCollected=formPasswordConfirm.getText().toString().trim();
-    }
+
     private void utilizedUI() {
-        formSubmitButton=findViewById(R.id.formSubmitButton);
-        formName=findViewById(R.id.formName);
-        formEmail=findViewById(R.id.formEmail);
-        formMobile=findViewById(R.id.formMobile);
-        formPresentAddress=findViewById(R.id.formPresentAddress);
-        formPermanentAddress=findViewById(R.id.formPermanentAddress);
-        formGroup=findViewById(R.id.formGroup);
-        formNameFather=findViewById(R.id.formNameFather);
-        formNameMother=findViewById(R.id.formNameMother);
-        formNameSpouse=findViewById(R.id.formNameSpouse);
-        formNumberOfChild=findViewById(R.id.formNumberOfChild);
-        formOccupation=findViewById(R.id.formOccupation);
-        formInstituteName=findViewById(R.id.formInstituteName);
-        formDesignation=findViewById(R.id.formDesignation);
-        formOfficeAddress=findViewById(R.id.formOfficeAddress);
-        formMemberType=findViewById(R.id.formMemberType);
-        formPassword=findViewById(R.id.formPassword);
-        formPasswordConfirm=findViewById(R.id.formPasswordConfirm);
+        formSubmitButton = findViewById(R.id.formSubmitButton);
+        formName = findViewById(R.id.formName);
+        formEmail = findViewById(R.id.formEmail);
+        formMobile = findViewById(R.id.formMobile);
+        formPresentAddress = findViewById(R.id.formPresentAddress);
+        formPermanentAddress = findViewById(R.id.formPermanentAddress);
+        formGroup = findViewById(R.id.formGroup);
+        formNameFather = findViewById(R.id.formNameFather);
+        formNameMother = findViewById(R.id.formNameMother);
+        formNameSpouse = findViewById(R.id.formNameSpouse);
+        formNumberOfChild = findViewById(R.id.formNumberOfChild);
+        formOccupation = findViewById(R.id.formOccupation);
+        formInstituteName = findViewById(R.id.formInstituteName);
+        formDesignation = findViewById(R.id.formDesignation);
+        formOfficeAddress = findViewById(R.id.formOfficeAddress);
+        formMemberType = findViewById(R.id.formMemberType);
+        formPassword = findViewById(R.id.formPassword);
+        formPasswordConfirm = findViewById(R.id.formPasswordConfirm);
     }
 }
