08ebc9060b87490527c69fc1d270c05daccad8b1
shakawat <shakawat_hossain@outlook.com>
2022-07-02 18:35:51 +0600

    just code update

--- app/src/main/java/ngdc/rajshahi/reunion/anotherHomeActivity/becomeAmember/Become_a_member_activity.java
+++ app/src/main/java/ngdc/rajshahi/reunion/anotherHomeActivity/becomeAmember/Become_a_member_activity.java
@@ -3,6 +3,7 @@ package ngdc.rajshahi.reunion.anotherHomeActivity.becomeAmember;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -12,24 +13,24 @@ import android.widget.Toast;
 import ngdc.rajshahi.reunion.R;
 
 public class Become_a_member_activity extends AppCompatActivity {
-    EditText formPasswordConfirm,
-            formPassword,
-            formName,
-            formEmail,
+    EditText formName,
             formMobile,
             formPresentAddress,
             formPermanentAddress,
+            formImageFileChooserFileName,
+            formEmail,
+            formPassword,
+            formPasswordConfirm,
             formGroup,
-            formNameFather,
-            formNameMother,
             formNameSpouse,
             formNumberOfChild,
             formOccupation,
             formInstituteName,
             formDesignation,
-            formOfficeAddress,
-            formMemberType;
-    Button formSubmitButton;
+            formOfficeAddress;
+
+    Button formSubmitButton,
+            formImageFileChooserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,71 +42,79 @@ public class Become_a_member_activity extends AppCompatActivity {
         this.setTitle("Become a Member");
         utilizedUI();
 
-        String formNameCollected = formName.getText().toString().trim();
-        String formEmailCollected = formEmail.getText().toString().trim();
-        String formMobileCollected = formMobile.getText().toString().trim();
-        String formPresentAddressCollected = formPresentAddress.getText().toString().trim();
-        String formPermanentAddressCollected = formPermanentAddress.getText().toString().trim();
-        String formGroupCollected = formGroup.getText().toString().trim();
-        String formNameFatherCollected = formNameFather.getText().toString().trim();
-        String formNameMotherCollected = formNameMother.getText().toString().trim();
-        String formNameSpouseCollected = formNameSpouse.getText().toString().trim();
-        String formNumberOfChildCollected = formNumberOfChild.getText().toString().trim();
-        String formOccupationCollected = formOccupation.getText().toString().trim();
-        String formInstituteNameCollected = formInstituteName.getText().toString().trim();
-        String formDesignationCollected = formDesignation.getText().toString().trim();
-        String formOfficeAddressCollected = formOfficeAddress.getText().toString().trim();
-        String formMemberTypeCollected = formMemberType.getText().toString().trim();
-        String formPasswordCollected = formPassword.getText().toString().trim();
-        String formPasswordConfirmCollected = formPasswordConfirm.getText().toString().trim();
+        formImageFileChooserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String imageLink ="";
+                formImageFileChooserFileName.setText(imageLink);
+            }
+        });
+
+        String formName_Collected = formName.getText().toString().trim();
+        String formMobile_Collected = formMobile.getText().toString().trim();
+        String formPresentAddress_Collected = formPresentAddress.getText().toString().trim();
+        String formPermanentAddress_Collected = formPermanentAddress.getText().toString().trim();
+        String formImageFileChooserFileName_Collected = formImageFileChooserFileName.getText().toString().trim();
+
+        String formEmail_Collected = formEmail.getText().toString().trim();
+        String formPassword_Collected = formPassword.getText().toString().trim();
+        String formPasswordConfirm_Collected = formPasswordConfirm.getText().toString().trim();
+
+        String formGroup_Collected = formGroup.getText().toString().trim();
+
+        String formNameSpouse_Collected = formNameSpouse.getText().toString().trim();
+        String formNumberOfChild_Collected = formNumberOfChild.getText().toString().trim();
+
+        String formOccupation_Collected = formOccupation.getText().toString().trim();
+        String formInstituteName_Collected = formInstituteName.getText().toString().trim();
+        String formDesignation_Collected = formDesignation.getText().toString().trim();
+        String formOfficeAddress_Collected = formOfficeAddress.getText().toString().trim();
+
 
         formSubmitButton.setOnClickListener(view -> {
-            if (formNameCollected.equals("") ||
-                    formEmailCollected.equals("") ||
-                    formMobileCollected.equals("") ||
-                    formPresentAddressCollected.equals("") ||
-                    formPermanentAddressCollected.equals("") ||
-                    formGroupCollected.equals("") ||
-                    formNameFatherCollected.equals("") ||
-                    formNameMotherCollected.equals("") ||
-                    formNameSpouseCollected.equals("") ||
-                    formNumberOfChildCollected.equals("") ||
-                    formOccupationCollected.equals("") ||
-                    formInstituteNameCollected.equals("") ||
-                    formDesignationCollected.equals("") ||
-                    formOfficeAddressCollected.equals("") ||
-                    formPasswordCollected.equals("") ||
-                    formPasswordConfirmCollected.equals("") ||
-                    formMemberTypeCollected.equals("")) {
-                if (!formPasswordCollected.equals(formPasswordConfirmCollected)) {
-                    Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Fill up all information", Toast.LENGTH_SHORT).show();
-                }
+            if (formName_Collected.equals("") ||
+                    formMobile_Collected.equals("") ||
+                    formPresentAddress_Collected.equals("") ||
+                    formPermanentAddress_Collected.equals("") ||
+                    formGroup_Collected.equals("") ||
+                    formOccupation_Collected.equals("") ||
+                    formInstituteName_Collected.equals("") ||
+                    formDesignation_Collected.equals("") ||
+                    formPassword_Collected.equals("") ||
+                    formPasswordConfirm_Collected.equals("")) {
+                Toast.makeText(this, "Fill up required information", Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(this, "Successful submit your data", Toast.LENGTH_SHORT).show();
+                if (!formPassword_Collected.equals(formPasswordConfirm_Collected)){
+                    Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(this, "Successful submit your data", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
     private void utilizedUI() {
         formSubmitButton = findViewById(R.id.formSubmitButton);
+
         formName = findViewById(R.id.formName);
-        formEmail = findViewById(R.id.formEmail);
         formMobile = findViewById(R.id.formMobile);
         formPresentAddress = findViewById(R.id.formPresentAddress);
         formPermanentAddress = findViewById(R.id.formPermanentAddress);
+        formImageFileChooserButton = findViewById(R.id.formImageFileChooserButton);
+        formEmail = findViewById(R.id.formEmail);
+        formPassword = findViewById(R.id.formPassword);
+        formPasswordConfirm = findViewById(R.id.formPasswordConfirm);
+
         formGroup = findViewById(R.id.formGroup);
-        formNameFather = findViewById(R.id.formNameFather);
-        formNameMother = findViewById(R.id.formNameMother);
+
         formNameSpouse = findViewById(R.id.formNameSpouse);
         formNumberOfChild = findViewById(R.id.formNumberOfChild);
+
         formOccupation = findViewById(R.id.formOccupation);
         formInstituteName = findViewById(R.id.formInstituteName);
         formDesignation = findViewById(R.id.formDesignation);
         formOfficeAddress = findViewById(R.id.formOfficeAddress);
-        formMemberType = findViewById(R.id.formMemberType);
-        formPassword = findViewById(R.id.formPassword);
-        formPasswordConfirm = findViewById(R.id.formPasswordConfirm);
     }
 }
