fc5027f75bcf4faaacc8564dac425a257b581439
Bengal PC <shakawat_hossain@outlook.com>
2022-08-31 12:44:52 +0600

    listener add with MainActivity

--- app/src/main/java/ngdc/rajshahi/reunion/MainActivity.java
+++ app/src/main/java/ngdc/rajshahi/reunion/MainActivity.java
@@ -6,18 +6,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
-
+import ngdc.rajshahi.reunion.databinding.ActivityMainBinding;
 import ngdc.rajshahi.reunion.homeActivity.HomePage;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.gifID.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(),HomePage.class)));
+
         Handler handler = new Handler();
         handler.postDelayed(() -> startActivity(new Intent(getApplicationContext(), HomePage.class)), 5000);
     }
