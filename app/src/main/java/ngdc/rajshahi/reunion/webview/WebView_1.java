7031422e35e269aff6b3f000a9cbf7457193244b
shakawat <shakawat_hossain@outlook.com>
2022-08-31 22:54:32 +0600

    update code

--- app/src/main/java/ngdc/rajshahi/reunion/webview/WebView_1.java
+++ app/src/main/java/ngdc/rajshahi/reunion/webview/WebView_1.java
@@ -3,12 +3,15 @@ package ngdc.rajshahi.reunion.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,11 +19,19 @@ import android.view.WindowManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.Toast;
+
 import java.util.Objects;
 
 import ngdc.rajshahi.reunion.MainActivity;
 import ngdc.rajshahi.reunion.R;
+import ngdc.rajshahi.reunion.anotherHomeActivity.aboutUs.AboutUsActivity;
+import ngdc.rajshahi.reunion.anotherHomeActivity.becomeAmember.Become_a_member_activity;
+import ngdc.rajshahi.reunion.anotherHomeActivity.donarList.DonarListActivity;
+import ngdc.rajshahi.reunion.anotherHomeActivity.memberList.MemberListActivity;
 import ngdc.rajshahi.reunion.databinding.ActivityWebView1Binding;
+import ngdc.rajshahi.reunion.homeActivity.HomePage;
+import ngdc.rajshahi.reunion.payment.PaymentActivity;
 
 public class WebView_1 extends AppCompatActivity {
     ActivityWebView1Binding binding;
@@ -155,6 +166,37 @@ public class WebView_1 extends AppCompatActivity {
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
             binding.progressBar.setVisibility(View.GONE);
+            Log.e("TAG", "onPageFinished: "+url);
+
+            String webUrl_base="https://www.ngdcr90.org/";  //homepage activity
+            String webUrl_about="https://www.ngdcr90.org/about";  //about activity
+            String webUrl_memberList="https://www.ngdcr90.org/member-list";  //member list activity
+            String webUrl_member_register="https://www.ngdcr90.org/member-register";  //become a member activity
+            String webUrl_get_user_payment="https://www.ngdcr90.org/get-user-payment";  //payment activity
+            String webUrl_donation="https://www.ngdcr90.org/donation"; //Homepage
+            String webUrl_donation_list="https://www.ngdcr90.org/donation-list";  //donar list activity
+            String webUrl_gallery="https://www.ngdcr90.org/gallery"; //webview
+
+            String member_store= "https://www.ngdcr90.org/member-register-store";
+            String payment_store= "https://www.ngdcr90.org/user-payment/store";
+
+            if (url.equals(member_store)){
+                startActivity(new Intent(getApplicationContext(), HomePage.class));
+                Toast.makeText(WebView_1.this, "Registration Success", Toast.LENGTH_LONG).show();
+            }
+            if (url.equals(payment_store)){
+                startActivity(new Intent(getApplicationContext(), HomePage.class));
+                Toast.makeText(WebView_1.this, "Payment Success", Toast.LENGTH_LONG).show();
+            }
+//            //redirect url only from here
+//            if (url.equals(webUrl_base)){startActivity(new Intent(getApplicationContext(),HomePage.class));}
+//            if (url.equals(webUrl_about)){startActivity(new Intent(getApplicationContext(), AboutUsActivity.class));}
+//            if (url.equals(webUrl_memberList)){startActivity(new Intent(getApplicationContext(), MemberListActivity.class));}
+//            if (url.equals(webUrl_member_register)){startActivity(new Intent(getApplicationContext(), HomePage.class));}
+//            if (url.equals(webUrl_get_user_payment)){startActivity(new Intent(getApplicationContext(), HomePage.class));}
+//            if (url.equals(webUrl_donation)){startActivity(new Intent(getApplicationContext(), HomePage.class));}
+//            if (url.equals(webUrl_donation_list)){startActivity(new Intent(getApplicationContext(), DonarListActivity.class));}
+//            if (url.equals(webUrl_gallery)){startActivity(new Intent(getApplicationContext(), HomePage.class));}
         }
     }
 
@@ -164,5 +206,14 @@ public class WebView_1 extends AppCompatActivity {
             binding.webviewId.goBack();
             return true;
         }return super.onKeyDown(keyCode, event);
+    }
+
+    public void goToWebView(String url, String title, String whereFrom, Context mainActivity, Activity targetActivity){
+        Intent intent = new Intent(mainActivity, targetActivity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(getResources().getString(R.string.send_url), url);
+        intent.putExtra(getResources().getString(R.string.send_title), title);
+        intent.putExtra(getResources().getString(R.string.where_from), whereFrom);
+        startActivity(intent);
     }
 }
